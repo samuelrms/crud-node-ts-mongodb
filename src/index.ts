7d3b7ba987b70e9818b41ae33c3e41c7b0fef684
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-09 14:25:24 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("🔥 Start server 🔥");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 3030;
+
+app.get("/", (req, res) => {
+  res.send(`🔥 Start in Port = ${port} 🔥`);
+});
+
+app.listen(port, () => console.log(`🔥 Start server port = ${port} 🔥`));
