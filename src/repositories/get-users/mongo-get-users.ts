c10c3f42b24443de52e80938abc9774654bef87d
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-09 17:10:10 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,17 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Samuel",
-        lastName: "Ramos",
-        birth: new Date(2000, 1, 9),
-        cpf: "11377733670",
-        email: "samuelaoliveiraramos@gmail.com",
-        password: "1234",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...user }) => ({
+      ...user,
+      id: _id.toHexString(),
+    }));
   }
 }
