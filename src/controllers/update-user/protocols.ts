93b6f7db0100b70dbd0f3c8e67bd4f6533661941
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-10 17:21:12 -0300

    feat: add updateUserController witth validations

--- src/controllers/update-user/protocols.ts
+++ src/controllers/update-user/protocols.ts
@@ -1,8 +1,13 @@
 import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
 
 export type UserPropertiesToOmit = "id" | "cpf" | "email" | "birth";
 
 export type UpdateUserParams = Partial<Omit<User, UserPropertiesToOmit>>;
+
+export interface IUpdateUserController {
+  handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>>;
+}
 
 export interface IUpdateUserRepository {
   updateUser(id: string, params: UpdateUserParams): Promise<User>;
